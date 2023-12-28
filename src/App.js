f2e4cd7154f9fdfbbbb4e3c743ca603fd58d7ecf
Nathanael <nathanaelniyogushimwa@gmail.com>
2023-12-28 04:08:40 -0800

    leave from form to ather example

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import  Loginform from "./components/loginform.js";
+
+import { useState } from 'react';
 import './App.css';
 function App() {
-
+  const [showtext, setShowtext] = useState(true);
+  const hideshow = () => {
+    setShowtext(!showtext)
+  }
   return (
+       
     <div className="App">
-     <Loginform />
+      <button onClick={hideshow}>how/hide</button>
+      {showtext === true && <h1>hello my name is nathanael</h1>}
     </div>
   );
 };
--- src/components/loginform.js
+++ src/components/loginform.js
@@ -2,22 +2,21 @@ import React from "react";
 import "./loginform.css"
 
 const loginform = () => {
+ 
     return (
-        <div className="cover">
-            <h1>Login Form</h1>
-            <input type="text" placeholder="username" />
-            <input type="password" placeholder="password" />
-
-            <div className="login-btn">Login</div>
-
-            <p className="text">or login using</p>
-
-            <div className="alt-login">
-                <div className="facebook"></div>
-                <div className="google"></div>
-            </div>
-
+       <div className="cover">
+        <h1>Login Form</h1>
+        <input type="text" placeholder="UserName"/>
+        <input type="password" placeholder="password" />
+        <div className="login-btn">
+            login
         </div>
+        <p className="text">or login using</p>
+        <div className="alt-login">
+            <div className="google"></div>
+            <div className="facebook"></div>
+        </div>
+       </div>
     )
 }
 export default loginform;
